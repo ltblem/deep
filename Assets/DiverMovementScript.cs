eea9a3106be622f9ab7cb67b274a837dadf8c5b6
ltblem <jamsieh@icloud.com>
2023-05-06 20:53:26 +0100

    Added a player with movement, no texture yet

---
+++ Assets/DiverMovementScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiverMovementScript : MonoBehaviour
+{
+    public float diverSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, diverSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, -diverSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(diverSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(-diverSpeed, 0);
+        }
+    }
+}
